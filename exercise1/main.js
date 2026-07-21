771d7cb438116e318f1f98948ad4ae7b7e7012e0
abdirahmanciid <abdirahmanciid11@gmail.com>
2026-07-21 17:04:21 +0300

    submited js exercise1

---
+++ exercise1/main.js
@@ -0,0 +1,37 @@
+let x =7;
+let y =3;
+
+
+// addtion
+console.log(x+y)
+
+
+ x =7;
+ y =3;
+// Subtraction
+console.log(x-y)
+
+
+ x =7;
+ y =3;
+// // Multiplication
+console.log(x*y)
+
+
+ x =7;
+ y =3;
+// // division
+console.log(x/y)
+
+
+
+
+ x =7;
+ y =3;
+//  Modulus
+console.log(x%y)
+
+
+
+
+
