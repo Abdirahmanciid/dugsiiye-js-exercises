0a1bcb1ec9f35f1a68e66f1dce2d0bc10ade6d0b
abdirahmanciid <abdirahmanciid11@gmail.com>
2026-07-22 08:27:48 +0300

    submited ex2 and imgages folder

---
+++ exercise2/main.js
@@ -0,0 +1,43 @@
+let a =15;
+let b="15";
+
+
+// equal
+console.log(a==b)
+
+
+// not equal
+console.log(a!=b)
+
+
+
+
+// strick equal
+console.log(a===b)
+
+
+
+
+// strick not equal
+console.log(a!==b)
+
+
+
+
+// greater than
+console.log(a>b)
+
+
+// less than
+console.log(a<b)
+
+
+// greater or equal
+console.log(a>=b)
+
+
+// less than or equal
+console.log(a<=b)
+
+
+
