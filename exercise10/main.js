663ee9441cac8efb10865a9d596d954835756bf3
abdirahmanciid <abdirahmanciid11@gmail.com>
2026-07-23 19:40:00 +0300

    submited ex10

---
+++ exercise10/main.js
@@ -0,0 +1,15 @@
+let library = [
+    {
+        
+        author: "farah",
+        title: "cyrpto",
+        year: 2026
+    },
+    {
+    author: "deek",
+    title: "AI",
+    year: 2025,
+    }
+]
+console.log(library[0].title)
+console.log(library[1].author)
