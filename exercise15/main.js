c7d706d955172a4946d121d6d47d92a867757947
abdirahmanciid <abdirahmanciid11@gmail.com>
2026-07-25 17:53:45 +0300

    submited ex15

---
+++ exercise15/main.js
@@ -0,0 +1,32 @@
+
+console.log("properties and values of each person:")
+const people=[
+
+    {name:"jama", age: 22, city:"burao"},
+    {name:"najma", age: 19, city:"hargaysa"},
+    {name:"abdi", age: 30, city:"berbera"},
+    "----------"
+];
+for( let list of  people){
+    console.log(list)
+}
+const people2=[
+    {name:"ahmed", age: 20, city:"gabilay"},
+    {name:"farah", age: 18, city:"ceerigaabo"},
+    {name:"abdi", age: 21, city:"wajale"},
+    "----------"
+];
+for( let list2 of  people2){
+    console.log(list2)
+}
+
+
+const people3=[
+    {name:"saleban", age: 40, city:"caynaba"},
+    {name:"jamal", age: 39, city:"elfwayn"},
+    {name:"deeq", age: 20, city:"harta"},
+    
+];
+for( let list3 of  people3){
+    console.log(list3)
+}
