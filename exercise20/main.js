ad52f5d654a96d8f899c12c4734f45b575f2b24e
abdirahmanciid <abdirahmanciid11@gmail.com>
2026-07-28 09:19:43 +0300

    submited ex20

---
+++ exercise20/main.js
@@ -0,0 +1,12 @@
+// let fruits=["apple", "banana", "cherry"]
+// let newFruits = fruits.map((list)=> list, length)
+// console.log(newFruits)
+
+let fruits=["apple", "banana", "cherry"]
+let newFruits = fruits.map((list)=> list.split(""))
+
+console.log(newFruits)
+
+console.log(newFruits[0]);
+console.log(newFruits[1]);
+console.log(newFruits[2]);
