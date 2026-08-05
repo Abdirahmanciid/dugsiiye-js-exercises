8f335268bdc49fad2fc77a0fed03fee140dce2bf
abdirahmanciid <abdirahmanciid11@gmail.com>
2026-08-05 11:04:58 +0300

    submited exercises 32 and 33 js

---
+++ exercise32/main.js
@@ -0,0 +1,10 @@
+const header = document.querySelector("#header")
+console.log(header);
+
+const   btext =document.querySelectorAll(".b-text")
+console.log(btext);
+
+
+
+const buttons = document.querySelectorAll("button")
+console.log(buttons)
---
+++ exercise33/main.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("#header")
+const text = document.querySelector(".text")
+// console.log(header)
+
+
+function changeContent(){
+    header.textContent = "hey whatsup";
+}
+
+function changeElement(){
+text.innerHTML= "<b>Lorem ipsum dolor sit amet</b>";
+}
